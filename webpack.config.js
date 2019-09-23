27b2dda7382eec7dd0ec27b25f1db50ac1976759
Mina Amir <mina.amir25@yahoo.com>
2019-09-23 02:35:12 +0200

    add hot reload to html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require("path")
 const webpack = require("webpack")
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackHotPlugin = require('html-webpack-hot-plugin')
+const htmlHotPlugin = new HtmlWebpackHotPlugin()
 const webpack_rules = []
 const weabck_plugins = []
 const webpackOption = {
     mode:'development',
     entry: {
         main: "./src/js/main.js", 
-        slider: ["./src/js/slider.js", "./src/css/awesomeSlider.scss", "./index.html"],
+        slider: ["./src/js/slider.js", "./src/css/awesomeSlider.scss"],
     },
     output: {
         filename: "js/[name].min.js",
@@ -17,13 +20,14 @@ const webpackOption = {
         rules: webpack_rules
     },
     plugins: weabck_plugins,
-    // devServer:{
-    //     contentBase: './',
-    //     publicPath: './dist',
-    //     inline: true,
-    //     hot: true,
-    // },
-};
+    devServer: {
+        before(app, server) {
+          // This step is curcial. DevServer is needed to send reload message to opened page.
+          // Without this step, the update of HtmlWebpackHotPlugin will be omitted and you will need to refresh the page manually.
+          htmlHotPlugin.setDevServer(server)
+        }
+    }
+}
 let babelLoader = {
     test: /\.js$/,
     exclude: /(node_modules|bower_components)/,
@@ -57,15 +61,18 @@ let sassCompiler = {
         }
     ]
 }
-let htmlWatch = {
-    test: /\.html$/,
-    loader: "raw-loader"
-}
+let htmlWatch = new HtmlWebpackPlugin({
+    filename: './index.html',
+    template: './index.html',
+})
+  // the instance of this plugin must be placed after instances of HtmlWebpackPlugin
+let htmlHotReload = htmlHotPlugin
 let copyPlugin = new CopyWebpackPlugin([
     { from: 'src/images', to: 'images' }
 ])
 webpack_rules.push(babelLoader)
 webpack_rules.push(sassCompiler)
-webpack_rules.push(htmlWatch)
+weabck_plugins.push(htmlWatch)
+weabck_plugins.push(htmlHotReload)
 weabck_plugins.push(copyPlugin)
 module.exports = webpackOption
